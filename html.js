65650f5aeda65a05d6eacafb8459cf72cff9210a
Alex Barry <alex.barry@gmail.com>
2017-11-29 12:16:39 -0500

    Filled out and semi-documented recommended.js, one of my rules was more html-y, so I added vehikl/html to the configs.

---
+++ html.js
@@ -0,0 +1,6 @@
+module.exports = {
+  rules: {
+    "no-script-url": "error", // Don't allow <a href="javascript:<js code here>"></a>, use onclick/event listeners instead
+  }
+}
+
--- recommended.js
+++ recommended.js
@@ -1,4 +1,41 @@
+/*
+ * Recommended rules
+ *
+ * These rules should be about eliminating javascript errors and common pitfalls.
+ */
 module.exports = {
   rules: {
+    "no-template-curly-in-string": "error", // If it's not a template string, flag template-style code in that string
+    "array-callback-return": "error", // Array.map/reduce/every/some/etc... requires return statements
+    "consistent-return": "warning", // If one branch of a function returns a value, all other branches should also return a value
+    "curly": ["error", "multi", "consistent"], // Avoid curlies for single-line blocks, but in a chain, if one block needs curlies, force all other blocks in the chain to use curlies
+    "no-caller": "error", // Don't use arguments.caller or arguments.calless
+    "no-else-return": "warning", // If you have an if and else, and the if has an explicit return statement, you can avoid using an else condition
+    "no-extra-bind": "warning", // Don't bind if the function isn't using `this`
+    "no-implicit-globals": "warning", // Don't put stuff in globals
+    "no-invalid-this": "warning", // Don't use this if you're not in code being ran from a class-like object
+    "no-labels": "error", // Don't use labels.
+    "no-new-wrappers": "error", // Don't use new on type primitives (String, Number, Boolean, Object)
+    "no-new-func": "error", // Don't use new on a function, that's why we have classes
+    "no-param-reassign": "warning", // Don't change the value of a parameter in a function, since this is a side-effect
+    "no-return-assign": "error", // Don't allow assignment in a return
+    "no-return-await": "warning", // Don't allow async functions to immediately return an await.
+    "no-self-compare": "error", // Don't allow comparisons to self, which is usually a refactor bug
+    "no-unmodified-loop-condition": "error", // Don't allow infinite loops, and prefer loops with variables rather than breaks
+    "no-useless-concat": "error", // Don't concat things in code when it can be done manually
+    "no-useless-call": "error", // Don't allow .call or .apply unless there is a good reason
+    "require-await": "warning", // If you have an async function, it should have at least one await
+    "no-shadow": "warning", // Don't have same-name variables in various levels of scope (can be misleading)
+    "no-shadow-restricted-names": "error", // Don't use variable names that are restricted (ie undefined, null, etc.)
+    "no-use-before-define": "error", // Don't use variables before they are defined
+    "init-declarations": ["error", "always"], // If you're creating a variable, it must start with a value
+    "dot-location": ["error", "property"], // Multi-line chaining of an object should have dots on the next line
+    "dot-notation": ["error", { "allowKeywords": true }], // Prefer using dot notation .key and not ["key"].
+    "eqeqeq": "error", // Always use type-safe comparisons
+    "no-lone-blocks": "error", // Don't use blocks unnecessarily
+    "yoda": ["error", "never"], // Use variable <condition> value, and not the opposite
+    "no-loop-func": "error", // If you're creating functions in a loop, make sure the variables they use are properly scoped with let or const
+    "no-magic-numbers": ["warning", { "ignore": [1] }], // You must create constants to better identify numbers.  Ie `const a = test - 10` is bad because there is no context to what 10 actually means.
+    "no-sequences": "error", // Don't allow comma-separated code for initialization, except in for loops.
   }
 }
