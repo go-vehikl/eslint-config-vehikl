3e3543052827d1f58d4974772292e9aaf15cee5c
Alex Barry <alex.barry@gmail.com>
2017-11-30 16:14:28 -0500

    Will get to fixing up html.js, added example,eslintrc file.

---
+++ eslintrc.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "extends": ["eslint:recommended", "vehikl/recommended"],
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module"
+  },
+  "env": {
+    "es6": true
+  }
+}
--- html.js
+++ html.js
@@ -1,3 +1,4 @@
+// TODO: Fixme
 module.exports = {
   rules: {
     "no-script-url": "error", // Don't allow <a href="javascript:<js code here>"></a>, use onclick/event listeners instead
